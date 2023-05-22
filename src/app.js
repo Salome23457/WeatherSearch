5e499813060e874ec2bdb446b083e89527d90d6e
Salome <salom.zonou@yahoo.fr>
2023-05-22 11:36:33 +0100

    AddApi

---
+++ src/app.js
@@ -0,0 +1,12 @@
+let apiKey = "3b7t38c5bd8502c1f10a4o47b15a6c8a";
+
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid={API key}&units=metric";
+
+function displayTemp(response) {
+  console.log(response.data.main.temp);
+  let temperatureEl = document.querySelector("#temperature");
+  temperatureEl.innerHTML = Math.round(response.data.main.temp);
+}
+
+axios.get(apiUrl).then(displayTemp);
