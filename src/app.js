1227b8c59f3b10249d7911ab5112a48d67238d57
Salome <salom.zonou@yahoo.fr>
2023-05-22 13:59:37 +0100

    addIcon

--- src/app.js
+++ src/app.js
@@ -1,13 +1,52 @@
 let apiKey = "bb0df6985c2eab6a171d64a6bacbb4e1";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours} `;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes} `;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}: ${minutes}`;
+}
 
 function displayTemp(response) {
   console.log(response.data.main.temp);
   let temperatureEl = document.querySelector("#temperature");
   let cityEl = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  let dateEl = document.querySelector("#date");
+  let icon = document.querySelector("#icon");
   temperatureEl.innerHTML = Math.round(response.data.main.temp);
   cityEl.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  dateEl.innerHTML = formatDate(response.data.dt * 1000);
+  icon.setAttribute(
+    "src",
+    ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 axios.get(apiUrl).then(displayTemp);
